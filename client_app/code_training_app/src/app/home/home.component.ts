a4849c1a8c1356094d1a2b11fe2136fb32f4c095
Piotr Gazda <pjgazda@interia.pl>
2024-08-22 12:20:39 +0200

    Move navbar and footer to separate components.

--- client_app/code_training_app/src/app/home/home.component.ts
+++ client_app/code_training_app/src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { QuizGridComponent } from "../quiz-grid/quiz-grid.component";
+import { NavbarComponent } from "../navbar/navbar.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, QuizGridComponent],
+  imports: [RouterLink, QuizGridComponent, NavbarComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
