339515b4f2ccea9e444441e3c32371a88d193c6c
gazda <pjgazda@interia.pl>
2025-04-28 16:02:13 +0200

    Add transition animations.

--- client_app/code_training_app/src/app/app.component.ts
+++ client_app/code_training_app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { QuestionListComponent } from './components/question-list/question-list.component';
 import { AuthService } from './services/auth.service';
 import { Router } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
 import { HomeComponent } from "./components/home/home.component";
+import { routeTransition } from './route-transitions';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +13,15 @@ import { HomeComponent } from "./components/home/home.component";
   imports: [RouterOutlet, QuestionListComponent, LoginComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  providers: [AuthService]
+  providers: [AuthService],
+  animations: [
+    routeTransition
+  ]
 })
 export class AppComponent {
   title = 'code_training_app';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, protected route: ActivatedRoute) { }
 
   navigateToLogin() {
     this.router.navigate(['/login']);
---
+++ client_app/code_training_app/src/app/route-transitions.ts
@@ -0,0 +1,20 @@
+import {trigger, query, style, transition, animate} from '@angular/animations';
+
+export const routeTransition = trigger('routeTransition',
+    [
+        transition('* => *', [
+            query(':enter',[
+                style({opacity: 0, scale: 0.9}),
+                ], 
+            {optional: true}),
+            query(':leave',[
+                animate('0.2s', style({opacity: 0, scale: 0.9})),
+                ], 
+            {optional: true}),
+            query(':enter',[
+                animate('0.2s', style({opacity: 1, scale: 1})),
+                ], 
+            {optional: true}),
+        ])
+    ]
+)
